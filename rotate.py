2db34cf9e63b991f27f731a90a6d49b47632294a
milky1210 <mitukifukumoto@gmail.com>
2024-04-05 22:49:33 +0900

    update

--- rotate.py
+++ rotate.py
@@ -1,5 +1,6 @@
 import cv2
 import argparse
+from shutil import copyfile
 from pathlib import Path
 
 # ArgumentParser オブジェクトの作成
@@ -25,15 +26,25 @@ if img is None:
     print('Load Err.')  
 
 output_base_path = str(Path(input_filename).with_suffix(""))
+# CSVファイルの元のパス
+base_csv_path = Path('BASE.CSV')
+
+
+# +0度回転させて保存
+cv2.imwrite(output_base_path + '_0.JPG', img)
+copyfile(base_csv_path, output_base_path + '_0.CSV')
 
 # +90度回転して保存
 rotated_90 = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-cv2.imwrite(output_base_path + '_90.jpg', rotated_90)
+cv2.imwrite(output_base_path + '_90.JPG', rotated_90)
+copyfile(base_csv_path, output_base_path + '_90.CSV')
 
 # +180度回転して保存
 rotated_180 = cv2.rotate(img, cv2.ROTATE_180)
-cv2.imwrite(output_base_path + '_180.jpg', rotated_180)
+cv2.imwrite(output_base_path + '_180.JPG', rotated_180)
+copyfile(base_csv_path, output_base_path + '_180.CSV')
 
 # +270度（または-90度）回転して保存
 rotated_270 = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-cv2.imwrite(output_base_path + '_270.jpg', rotated_270)
+cv2.imwrite(output_base_path + '_270.JPG', rotated_270)
+copyfile(base_csv_path, output_base_path + '_270.CSV')
