d2f124afb7ada45d462b9ee3a5b6b05b6e2e6eae
milky1210 <mitukifukumoto@gmail.com>
2024-04-05 23:22:19 +0900

    update

--- rotate.py
+++ rotate.py
@@ -8,12 +8,14 @@ parser = argparse.ArgumentParser(description='Process an image file.')
 
 # コマンドライン引数の追加（'--input' はオプション名、helpはその説明）
 parser.add_argument('--input', type=str, help='Path to the input image file.')
+parser.add_argument('--id', type=int, help='id of output file.')
 
 # 引数を解析
 args = parser.parse_args()
 
 # 入力ファイル名の取得
 input_filename = args.input
+id_ = args.id
 
 # 入力がなければ終わり
 if not input_filename:
@@ -25,26 +27,27 @@ img = cv2.imread(input_filename)
 if img is None:
     print('Load Err.')  
 
-output_base_path = str(Path(input_filename).with_suffix(""))
+output_base_path = "outputs/GOPR"
 # CSVファイルの元のパス
-base_csv_path = Path('BASE.CSV')
+base_csv_path = Path('src/BASE.CSV')
 
 
 # +0度回転させて保存
-cv2.imwrite(output_base_path + '_0.JPG', img)
-copyfile(base_csv_path, output_base_path + '_0.CSV')
+cv2.imwrite(output_base_path + f'{id_:04d}.JPG', img)
+copyfile(base_csv_path, output_base_path + f'{id_:04d}.CSV')
 
-# +90度回転して保存
-rotated_90 = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-cv2.imwrite(output_base_path + '_90.JPG', rotated_90)
-copyfile(base_csv_path, output_base_path + '_90.CSV')
+if False:
+    # +90度回転して保存
+    rotated_90 = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    cv2.imwrite(output_base_path + f'{id_+1:04d}.JPG', rotated_90)
+    copyfile(base_csv_path, output_base_path + f'{id_+1:04d}.CSV')
 
-# +180度回転して保存
-rotated_180 = cv2.rotate(img, cv2.ROTATE_180)
-cv2.imwrite(output_base_path + '_180.JPG', rotated_180)
-copyfile(base_csv_path, output_base_path + '_180.CSV')
+    # +180度回転して保存
+    rotated_180 = cv2.rotate(img, cv2.ROTATE_180)
+    cv2.imwrite(output_base_path + f'{id_+2:04d}.JPG', rotated_180)
+    copyfile(base_csv_path, output_base_path + f'{id_+2:04d}.CSV')
 
-# +270度（または-90度）回転して保存
-rotated_270 = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-cv2.imwrite(output_base_path + '_270.JPG', rotated_270)
-copyfile(base_csv_path, output_base_path + '_270.CSV')
+    # +270度（または-90度）回転して保存
+    rotated_270 = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    cv2.imwrite(output_base_path + f'{id_+3:04d}.JPG', rotated_270)
+    copyfile(base_csv_path, output_base_path + f'{id_+3:04d}.CSV')
